ab3e31c83b0d230cef21b6d18bb310e939b607a1
Ekeh Pearl <pearlekeh7@gmail>
2025-07-07 11:03:58 +0100

    fix the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
+// app/layout.tsx  (or app/layout.js)
+import type { Metadata } from "next";
 import "./globals.css";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import ScrollToTop from "./components/ScrollToTop";
-import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Pearl Ekeh – Frontend Dev",
   icons: {
-    icon: "/avatar/png",
+    icon: "/images/avatar.png",
   },
 };
 
